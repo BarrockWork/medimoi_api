c445b90b06d804f0d341457df860ea4a8a3b4fad
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2022-01-14 12:34:05 +0100

    [CONFIG]refacto de code // ajout de cmd start + SP

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require('dotenv').config({path:".env.local"});
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
+const path = require('path'); 
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');

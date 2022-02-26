daa5688b155b7fcdbdfba77936cfbdec90e05c61
SandRatiaray <s.ratiaray@gmail.com>
2022-02-26 13:01:58 +0100

    add routes for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+const action = () => {};
+
+
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/new', action);
+
+router.put('/:id', action);
+
+router.delete('/:id', action);
+
+router.get('/:id', action);
 
 module.exports = router;

10794025d91841d0aee1fef7695f145f7818c250
Yannis Baltus <yannis.baltus@outlook.com>
2022-03-16 17:41:48 +0100

    [KHYS-140][Contact_type] test unitaire

---
+++ src/__tests__/units_tests/contact_type.test.js
@@ -0,0 +1,26 @@
+/*
+ *   Link for the errors reference of prisma:
+ *   - https://www.prisma.io/docs/reference/api-reference/error-reference
+ */
+
+const Models = require('./../../models');
+const { createSlug } = require('./../../utils/requestHandler')
+const {testMaxLength} = require('./../../utils/testHandler')
+
+// Disconnect prisma after all of the tests
+afterAll(async () => {
+    await Models.$disconnect();
+})
+
+// Initialise a contact_type object
+const contactTypeDefault = {
+    name: 'Contact type',
+    nameSlug: createSlug('Contact type'),
+}
+
+/*
+ * Init the company test group
+ */
+describe("Contact_type unit testing", () => {
+    testMaxLength('ContactType', contactTypeDefault, 'name', 50);
+})

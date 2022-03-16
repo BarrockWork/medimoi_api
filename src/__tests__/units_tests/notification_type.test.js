69660f9b7742754551584ae62510495961fdafa9
Yannis Baltus <yannis.baltus@outlook.com>
2022-03-16 17:44:07 +0100

    [KHYS-140][Notification_type] test unitaire

---
+++ src/__tests__/units_tests/notification_type.test.js
@@ -0,0 +1,26 @@
+/*
+ *   Link for the errors reference of prisma:
+ *   - https://www.prisma.io/docs/reference/api-reference/error-reference
+ */
+
+const Models = require('./../../models');
+const { createSlug } = require('./../../utils/requestHandler')
+const {testMaxLength} = require('./../../utils/testHandler')
+
+// Disconnect prisma after all of the tests
+afterAll(async () => {
+    await Models.$disconnect();
+})
+
+// Initialise a NotificationType object
+const notificationTypeDefault = {
+    name: 'Notif type',
+    nameSlug: createSlug('Notif type'),
+}
+
+/*
+ * Init the NotificationType test group
+ */
+describe("NotificationType unit testing", () => {
+    testMaxLength('NotificationType', notificationTypeDefault, 'name', 50);
+})

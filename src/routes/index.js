ad725be1c04a4520116761352ed19f291d670528
Yannis Baltus <yannis.baltus@outlook.com>
2022-03-26 17:36:37 +0100

    [Config] Refactor the routes configuration

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,47 @@
-const Home = require('./home');
+const indexRouter = require('./home');
+const usersRouter = require('./users');
+const userTypeRouter = require('./user_type');
+const userNotificationRouter = require('./user_notification_type');
+const addressRoadTypeRouter = require('./address_road_type');
+const addressRouter = require('./address');
+const treatmentRouter = require('./treatment');
+const treatmentMediaRouter = require('./treatment_media');
+const treatmentDrugRouter = require('./treatment_drug');
+const treatmentPeriodicityRouter = require('./treatment_periodicity');
+const medicalAdministrationRouter = require('./medical_administration');
+const diseaseRouter = require('./disease');
+const diseaseTypeRouter = require('./disease_type');
+const drugRouter = require('./drug');
+const drugLevelRouter = require('./drug_level');
+const drugTypeRouter = require('./drug_type');
+const NotificationTypeRouter = require('./notification_type');
+const NotificationHistory = require('./notification_history');
+const ContactTypeRouter = require('./contact_type');
+const CompanyRouter = require('./company');
+const ContactRouter = require('./contact');
+const UserCompanyRouter = require('./user_company');
 
 module.exports = {
-  Home,
+  indexRouter,
+  usersRouter,
+  userTypeRouter,
+  userNotificationRouter,
+  addressRoadTypeRouter,
+  addressRouter,
+  treatmentRouter,
+  treatmentMediaRouter,
+  treatmentDrugRouter,
+  treatmentPeriodicityRouter,
+  medicalAdministrationRouter,
+  diseaseRouter,
+  diseaseTypeRouter,
+  drugRouter,
+  drugLevelRouter,
+  drugTypeRouter,
+  NotificationTypeRouter,
+  NotificationHistory,
+  ContactTypeRouter,
+  CompanyRouter,
+  ContactRouter,
+  UserCompanyRouter
 }

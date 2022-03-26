d600e4c2b0db63faace35ec49f135427d93354c2
SandRatiaray <s.ratiaray@gmail.com>
2022-03-26 19:59:02 +0100

    fix stash pop for removing data from merge

--- src/__tests__/functionnals_tests/user.test.js
+++ src/__tests__/functionnals_tests/user.test.js
@@ -1,12 +1,17 @@
+const request = require('supertest');
+const app = require('./../../app.js');
+
 describe('GET /users', () => {
-  it('respond with json', (done) => {
+  it('respond with empty array', (done) => {
     request(app)
       .get('/users')
       .set('Accept', 'application/json')
-      .expect(200)
+      .expect(404)
       .end(function (err, res) {
         if (err) return done(err);
         done();
       });
   });
 });
+
+describe('POST /users/:id', () => {})

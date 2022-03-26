b91c4ae70b563640c0ca4a85c732f027c9544a0d
Yannis Baltus <yannis.baltus@outlook.com>
2022-03-26 19:05:25 +0100

    [KHYS-142][Company] Functionals tests for the POSTS functions

--- src/__tests__/functionnals_tests/company.test.js
+++ src/__tests__/functionnals_tests/company.test.js
@@ -3,41 +3,90 @@
  *   - https://www.prisma.io/docs/reference/api-reference/error-reference
  */
 
+const supertest = require('supertest');
+const createServerTest = require("./../server_test");
 const Models = require('./../../models');
-const { createSlug } = require('./../../utils/requestHandler')
-const {testMaxLength} = require('./../../utils/testHandler')
-
-// jest.mock('@prisma/client', () => {
-//     return {
-//         PrismaClient: function() {
-//             return {
-//                 performance: {
-//                     create: jest.fn()
-//                 },
-//             };
-//         },
-//     };
-// })
-
+const {createSlug} = require("./../../utils/requestHandler");
+const {testMaxLength} = require('./../../utils/testHandler');
+const R = require('ramda');
 
 // Disconnect prisma after all of the tests
 afterAll(async () => {
     await Models.$disconnect();
 })
 
-// Initialise a company object
-const companyDefault = {
-    name: 'Test',
-    nameSlug: createSlug('Test'),
-    siret: '123456789',
-    tva: 'FR123456789'
-}
+// Initialize express server
+const appTest = createServerTest()
+
+// Initialise a list of company object
+const schemaObject = [
+    {
+        name: 'Company Test Functional',
+        siret: '123456789',
+        tva: 'FR123456789'
+    },
+    {
+        name: 'Company Test Functional medimoi',
+        siret: '123456789',
+        tva: 'FR123456789'
+    },
+    {
+        name: 'Company Test Functional medimoi 2',
+        siret: '123456789',
+        tva: 'FR123456789'
+    },
+]
 
 /*
  * Init the company test group
  */
-describe("Company unit testing", () => {
-    testMaxLength('Company', companyDefault, 'name', 50);
-    testMaxLength('Company', companyDefault, 'siret', 50);
-    testMaxLength('Company', companyDefault, 'tva', 50);
+describe("Company functional testing", () => {
+
+    test("POST - Company - createOne()", async () => {
+        // Clone the schemaObject[0] in order to avoid to modify the original
+        let cloneSchemaObject = R.clone(schemaObject[0]);
+
+        await supertest(appTest)
+            .post("/api/company/new")
+            .send(cloneSchemaObject)
+            .expect(200)
+            .then(async (response) => {
+                // Check the response
+                expect(response.body.nameSlug).toBe("company-test-functional")
+
+                // Check the data in the database
+                const company = await Models.Company.findUnique({
+                    where: {
+                        nameSlug: "company-test-functional"
+                    }
+                });
+                expect(company).toBeTruthy()
+                expect(company.nameSlug).toBe("company-test-functional")
+            })
+    })
+
+    test("POST - Company - createMany()", async () => {
+        // Clone the schemaObjects in order to avoid to modify the original
+        let cloneSchemaObjects = {
+            "entries": [R.clone(schemaObject[1]), R.clone(schemaObject[2])]
+        };
+        await supertest(appTest)
+            .post("/api/company/news")
+            .send(cloneSchemaObjects)
+            .expect(200)
+            .then(async (response) => {
+                // Check the response
+                expect(response.body.count).toEqual(2);
+
+                // Check the data in the database
+                const companies = await Models.Company.findMany({
+                    where: {
+                        nameSlug: {
+                            contains: 'medimoi'
+                        }
+                    }
+                });
+                expect(companies).toHaveLength(2);
+            })
+    })
 })

6c0f7ca84455ccdc65c8a41bbe09c4308f3e5c5e
Yannis Baltus <yannis.baltus@outlook.com>
2022-03-26 19:35:40 +0100

    [KHYS-142][Company]GET, PUT and DELETE functionals tests

--- src/__tests__/functionnals_tests/company.test.js
+++ src/__tests__/functionnals_tests/company.test.js
@@ -6,9 +6,12 @@
 const supertest = require('supertest');
 const createServerTest = require("./../server_test");
 const Models = require('./../../models');
-const {createSlug} = require("./../../utils/requestHandler");
-const {testMaxLength} = require('./../../utils/testHandler');
 const R = require('ramda');
+
+// Delete all record before starting the tests
+beforeAll( async () =>{
+    await Models.Company.deleteMany({});
+})
 
 // Disconnect prisma after all of the tests
 afterAll(async () => {
@@ -42,7 +45,7 @@ const schemaObject = [
  */
 describe("Company functional testing", () => {
 
-    test("POST - Company - createOne()", async () => {
+    test("POST - /api/company/new", async () => {
         // Clone the schemaObject[0] in order to avoid to modify the original
         let cloneSchemaObject = R.clone(schemaObject[0]);
 
@@ -65,7 +68,7 @@ describe("Company functional testing", () => {
             })
     })
 
-    test("POST - Company - createMany()", async () => {
+    test("POST - /api/company/news", async () => {
         // Clone the schemaObjects in order to avoid to modify the original
         let cloneSchemaObjects = {
             "entries": [R.clone(schemaObject[1]), R.clone(schemaObject[2])]
@@ -89,4 +92,68 @@ describe("Company functional testing", () => {
                 expect(companies).toHaveLength(2);
             })
     })
+
+    test("GET - /api/company/slug/:nameSlug", async () => {
+        // Clone the schemaObjects in order to avoid to modify the original
+        await supertest(appTest)
+            .get("/api/company/slug/company-test-functional")
+            .expect(200)
+            .then(async (response) => {
+                // Check the response
+                expect(response.body.nameSlug).toBe("company-test-functional")
+            })
+    })
+
+    test("GET - /api/company/all", async () => {
+        // Clone the schemaObjects in order to avoid to modify the original
+        await supertest(appTest)
+            .get("/api/company/all")
+            .expect(200)
+            .then(async (response) => {
+                // Check the response
+                expect(response.body.length).toBeGreaterThan(1)
+            })
+    })
+
+    test("PUT - /api/company/slug/:nameSlug", async () => {
+        // Clone the schemaObject in order to avoid to modify the original
+        let cloneSchemaObject = R.clone(schemaObject[0]);
+        cloneSchemaObject.name = "Company Edition"
+
+        await supertest(appTest)
+            .put("/api/company/slug/company-test-functional")
+            .send(cloneSchemaObject)
+            .expect(200)
+            .then(async (response) => {
+                // Check the response
+                expect(response.body.nameSlug).toBe("company-edition");
+
+                // Check the data in the database
+                const company = await Models.Company.findUnique({
+                    where: {
+                        nameSlug: "company-edition"
+                    }
+                });
+                expect(company.nameSlug).toBe("company-edition");
+            })
+    })
+
+    test("DELETE - /api/company/slug/:nameSlug", async () => {
+        await supertest(appTest)
+            .delete("/api/company/slug/company-test-functional-medimoi-2")
+            .expect(200)
+            .then(async (response) => {
+                // Check the response (prisma return the deleted object datas
+                expect(response.body.nameSlug).toBe("company-test-functional-medimoi-2");
+
+                // Check the data in the database
+                const company = await Models.Company.findUnique({
+                    where: {
+                        nameSlug: "company-test-functional-medimoi-2"
+                    }
+                });
+                expect(company).toBeNull();
+            })
+    })
+
 })

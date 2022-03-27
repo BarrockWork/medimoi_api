36491d73dd2986ff005e6b8348b6bb81caf43949
SandRatiaray <s.ratiaray@gmail.com>
2022-03-27 14:36:09 +0200

    Prisma Schema update and refacto Test

---
+++ src/__tests__/objectSchema_test.js
@@ -0,0 +1,47 @@
+const UserTypeSchemaObject = [
+  {
+    name: 'User Type Test',
+  },
+  {
+    name: 'User Type Test 2 medimoi',
+  },
+  {
+    name: 'User Type Test 3 medimoi',
+  },
+];
+
+const UserSchemaObject = {
+  firstName: 'john',
+  lastName: 'doe',
+  age: 30,
+  email: 'jd@medimoi.com',
+  password: 'password',
+  cellphone: '0123456789',
+  homephone: '0123456789',
+  role: 'user',
+  user_type_id: 1,
+};
+
+const AddressRoadTypeSchemaObject = {
+  name: 'Address Road Type Test',
+};
+
+// Initialise a list of address object
+const AddressSchemaObject = {
+  numberRoad: 1,
+  streetName: 'madame',
+  user_id: 1,
+  address_road_type_id: 1,
+  zipcode: '12345',
+  city: 'bouillante',
+  region: 'test',
+  country: 'France',
+  title: 'PC',
+};
+
+module.exports = {
+  AddressSchemaObject,
+  AddressRoadTypeSchemaObject,
+  UserSchemaObject,
+  UserTypeSchemaObject,
+};

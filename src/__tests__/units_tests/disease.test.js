f4e6511d12c04269c4520c15f2a7fe09525e32ae
hassan <derkaouihassan@gmail.com>
2022-03-31 23:24:40 +0200

    create test unit for disease & disease_type

---
+++ src/__tests__/units_tests/disease.test.js
@@ -0,0 +1,32 @@
+/*
+ *   Link for the errors reference of prisma:
+ *   - https://www.prisma.io/docs/reference/api-reference/error-reference
+ */
+
+const Models = require('./../../models');
+const { createSlug } = require('./../../utils/requestHandler')
+const {testMaxLength, testUniqueness} = require('./../../utils/testHandler')
+
+// Disconnect prisma after all of the tests
+afterAll(async () => {
+    await Models.$disconnect();
+})
+
+// Initialise a Disease object
+const DiseaseDefault = {
+    name: 'Disease Test',
+    nameSlug: createSlug('Disease Test'),
+    description: 'ceci est une description',
+    incubationPeriod: 'ceci est une incubation',
+    transmitting: 'transmission',
+    disease_type_id: 1,
+}
+
+/*
+ * Init the Disease test group
+ */
+describe("Disease unit testing", () => {
+    testMaxLength('Disease', DiseaseDefault, 'name', 255);
+    testMaxLength('Disease', DiseaseDefault, 'transmitting', 255);
+    testUniqueness('Disease', DiseaseDefault, 'nameSlug');
+})
---
+++ src/__tests__/units_tests/disease_type.test.js
@@ -0,0 +1,29 @@
+/*
+ *   Link for the errors reference of prisma:
+ *   - https://www.prisma.io/docs/reference/api-reference/error-reference
+ */
+
+const Models = require('./../../models');
+const { createSlug } = require('./../../utils/requestHandler')
+const {testMaxLength} = require('./../../utils/testHandler')
+const {testUniqueness} = require("../../utils/testHandler");
+
+// Disconnect prisma after all of the tests
+afterAll(async () => {
+    await Models.$disconnect();
+})
+
+// Initialise a DiseaseType object
+const DiseaseTypeDefault = {
+    name: 'DiseaseType Test',
+    nameSlug: createSlug('DiseaseType Test'),
+    description: 'ceci est une description',
+}
+
+/*
+ * Init the DiseaseType test group
+ */
+describe("Company unit testing", () => {
+    testMaxLength('DiseaseType', DiseaseTypeDefault, 'name', 100);
+    testUniqueness('DiseaseType', DiseaseTypeDefault, 'nameSlug');
+})

470290569ce6824c08f8da3280d5a63635eadc63
Coulibaly Karamoko <k.coulibaly@ecole-ipssi.net>
2022-04-11 12:50:46 +0200

    add medical administration functional test

--- src/__tests__/functionnals_tests/medical_administration.test.js
+++ src/__tests__/functionnals_tests/medical_administration.test.js
@@ -10,13 +10,25 @@ const R = require('ramda');
 
 // Delete all record before starting the tests
 beforeAll( async () =>{
-    await Models.medicalAdministration.deleteMany({});
+    await Models.medicalAdministration.deleteMany({
+        where:{
+            name:{
+                contains: "Medical Administration Functional"
+            }
+        }
+    });
 });
 
 
 // Disconnect prisma after all of the tests
 afterAll(async () => {
-    await Models.medicalAdministration.deleteMany({});
+    await Models.medicalAdministration.deleteMany({
+        where:{
+            name:{
+                contains: "Medical Administration Functional"
+            }
+        }
+    });
     await Models.$disconnect();
 });
 
@@ -25,13 +37,13 @@ const appTest = createServerTest();
 // Initialise a list of company object
 const schemaObject = [
     {
-        name: 'Medical Administration Test Functional',
+        name: 'Medical Administration Functional',
     },
     {
-        name: 'Medical Administration Test Functional medimoi',
+        name: 'Medical Administration Functional medimoi',
     },
     {
-        name: 'Medical Administration Test Functional medimoi 2',
+        name: 'Medical Administration Functional medimoi 2',
     }
 ];
 
@@ -48,16 +60,16 @@ describe("Medical Administration functional testing", () => {
         .expect(200)
         .then(async (response) => {
             // Check the response
-            expect(response.body.nameSlug).toBe("medical-administration-test-functional");
+            expect(response.body.nameSlug).toBe("medical-administration-functional");
 
             // Check the data in the database
             const medicalAdministration = await Models.medicalAdministration.findUnique({
                 where: {
-                    nameSlug: "medical-administration-test-functional"
+                    nameSlug: "medical-administration-functional"
                 }
             });
             expect(medicalAdministration).toBeTruthy();
-            expect(medicalAdministration.name).toBe("Medical Administration Test Functional");
+            expect(medicalAdministration.name).toBe("Medical Administration Functional");
         });
     });
 
@@ -74,74 +86,151 @@ describe("Medical Administration functional testing", () => {
             // check the response
             expect(response.body.count).toBe(2);
 
-            const medicalAdministrations = await Models.medicalAdministration.findMany({});
-            expect(medicalAdministrations.length).toBe(3);
+            const medicalAdministrations = await Models.medicalAdministration.findMany({
+                where:{
+                    name:{
+                        contains: "Functional medimoi"
+                    }
+                }
+            });
+            expect(medicalAdministrations.length).toBe(2);
         });
     });
 
     test('GET - /api/medical_administrations/slug/:nameSlug', async () => {
+        await Models.medicalAdministration.create({
+            data:{
+                name: 'Medical Administration Functional Get',
+                nameSlug: 'medical-administration-functional-get'
+            }
+        })
+        await Models.$disconnect();
         await supertest(appTest)
-        .get('/api/medical_administrations/slug/medical-administration-test-functional')
+        .get('/api/medical_administrations/slug/medical-administration-functional-get')
         .expect(200)
         .then(async (response) => {
             // check the response
-            expect(response.body.name).toBe("Medical Administration Test Functional");
+            expect(response.body.name).toBe("Medical Administration Functional Get");
+        })
+        .then(async () => {
+            await Models.medicalAdministration.delete({
+                where:{
+                    nameSlug: 'medical-administration-functional-get'
+                }
+            })
         });
     });
 
     test('GET - /api/medical_administrations/all', async () => {
+        await Models.medicalAdministration.createMany({
+            data:[
+                {
+                    name: 'Medical Administration Functional Get Many 1',
+                    nameSlug: 'medical-administration-functional-get-many-1'
+                },
+                {
+                    name: 'Medical Administration Functional Get Many 2',
+                    nameSlug: 'medical-administration-functional-get-many-2'
+                },
+                {
+                    name: 'Medical Administration Functional Get Many 3',
+                    nameSlug: 'medical-administration-functional-get-many-3'
+                },
+            ]
+        });
+        await Models.$disconnect();
         await supertest(appTest)
         .get('/api/medical_administrations/all')
         .expect(200)
         .then(async (response) => {
             // check the response
-            expect(response.body.length).toBe(3);
-        });
+            console.log(response.body);
+            expect(response.body.length).toBeGreaterThan(2);
+        })
+        .then(async ()=>{
+            await Models.medicalAdministration.deleteMany({
+                where:{
+                    name:{
+                        contains: "Medical Administration Functional Get Many"
+                    }
+                }
+            });
+            const medicalAdministrations = await Models.medicalAdministration.findMany({
+                where:{
+                    name:{
+                        contains: "Medical Administration Functional"
+                    }
+                }
+            });
+            console.log(medicalAdministrations);
+        })
     });
 
     test('PUT - /api/medical_administrations/slug/nameSlug', async () => {
+
+        await Models.medicalAdministration.create({
+            data:{
+                name: 'Medical Administration Functional To Update',
+                nameSlug: 'medical-administration-functional-to-update'
+            }
+        });
+        await Models.$disconnect();
         // Clone the schemaObject in order to avoid to modify the original
         let cloneSchemaObject = R.clone(schemaObject[0]);
-        cloneSchemaObject.name = "Medical updated";
+        cloneSchemaObject.name = "Medical Administration Functional updated";
 
         await supertest(appTest)
-        .put('/api/medical_administrations/slug/medical-administration-test-functional')
+        .put('/api/medical_administrations/slug/medical-administration-functional-to-update')
         .send(cloneSchemaObject)
         .expect(200)
         .then(async (response) => {
             // check the response
-            expect(response.body.name).toBe("Medical updated");
-
-            // check the data in the database
-            const medicalAdministration = await Models.medicalAdministration.findUnique({
-                where: {
-                    nameSlug: "medical-updated"
+            console.log(response.body);
+            expect(response.body.name).toBe("Medical Administration Functional updated");
+        })
+        .then(async () => {
+            await Models.medicalAdministration.delete({
+                where:{
+                    nameSlug: 'medical-administration-functional-updated'
                 }
             });
-            expect(medicalAdministration).toBeTruthy();
-            expect(medicalAdministration.name).toBe("Medical updated");
-            expect(medicalAdministration.nameSlug).toBe("medical-updated");
         })
+        .catch((err) => {
+            console.log(err);
+        })
+        .finally(Models.$disconnect());
     });
 
     test('DELETE - /api/medical_administrations/slug/:nameSlug', async () => {
-        await supertest(appTest)
-        .delete('/api/medical_administrations/slug/medical-administration-test-functional-medimoi-2')
-        .expect(200)
-        .then(async (response) => {
-            // check the response 
-            expect(response.body.name).toBe("Medical Administration Test Functional medimoi 2");
-            const medicalAdministrations = await Models.medicalAdministration.findMany({});
-            expect(medicalAdministrations.length).toBe(2);
+        await Models.medicalAdministration.createMany({
+            data:[
+                {
+                    name: 'Medical Administration Functional Medimoi To Delete',
+                    nameSlug: 'medical-administration-functional-medimoi-to-delete'
+                },
+                {
+                    name: 'Medical Administration Functional Medimoi To Delete 2',
+                    nameSlug: 'medical-administration-functional-medimoi-to-delete-2'
+                }
+            ]
         });
+        await Models.$disconnect();
         await supertest(appTest)
-        .delete('/api/medical_administrations/slug/medical-administration-test-functional-medimoi')
+        .delete('/api/medical_administrations/slug/medical-administration-functional-medimoi-to-delete')
         .expect(200)
         .then(async (response) => {
             // check the response 
-            expect(response.body.name).toBe("Medical Administration Test Functional medimoi");
-            const medicalAdministrations = await Models.medicalAdministration.findMany({});
+            expect(response.body.name).toBe("Medical Administration Functional Medimoi To Delete");
+            const medicalAdministrations = await Models.medicalAdministration.findMany({
+                where:{
+                    name:{
+                        contains: "Medical Administration Functional Medimoi To Delete"
+                    }
+                }
+            });
+            console.log(medicalAdministrations);
+            // test que l'élément à bien été supprimé
             expect(medicalAdministrations.length).toBe(1);
-        });
+        })
     });
 });

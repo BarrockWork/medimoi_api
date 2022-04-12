3aa394d69fff4b3a04b6d956bd4c1574d7ead56a
Coulibaly Karamoko <k.coulibaly@ecole-ipssi.net>
2022-04-12 15:33:18 +0200

    remove console log

--- src/controllers/TreatmentMediaController.js
+++ src/controllers/TreatmentMediaController.js
@@ -137,7 +137,7 @@ const updateTreatmentMedia = async (req, res) => {
 
 // Delete function
 const deleteTreatmentMedia = async (req, res) => {
-    console.log("deleteTreatmentMedia");
+    // console.log("deleteTreatmentMedia");
     const {id} = req.params;
     try {
         await Models.treatmentMedia.delete({
@@ -151,7 +151,7 @@ const deleteTreatmentMedia = async (req, res) => {
             message: `Treatment media with id ${id} was deleted`
         });
     } catch (error) {
-        console.log(error);
+        // console.log(error);
         res.status(400).json({success: false})
     }
 }

f43ee70c3c390c49993a8419ff07bc1ef7dae150
hassan <derkaouihassan@gmail.com>
2022-05-06 01:54:52 +0200

    add some fixtures

---
+++ prisma/fixtures/drug_levels.js
@@ -0,0 +1,16 @@
+const drugLevels = [
+    {
+        "level": 1,
+        "description": "niveau 1"
+    },
+    {
+        "level": 2,
+        "description": "niveau 2"
+    },
+    {
+        "level": 3,
+        "description": "niveau 3"
+    }
+];
+
+module.exports = drugLevels;
---
+++ prisma/fixtures/drugs.js
@@ -0,0 +1,18 @@
+const drugs = [
+    {
+        "name": "test",
+        "description": "test",
+        "isPrescription": true
+    },
+    {
+        "name": "voltarene",
+        "description": "pommade ",
+        "isPrescription": true
+    },
+    {
+        "name": "toplexis",
+        "description": "sirop pour la toux",
+        "isPrescription": true
+    }
+]
+module.exports = drugs;

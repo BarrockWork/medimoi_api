9820ea566ec3d6795c05d3af468107ee0e1c33eb
SandRatiaray <s.ratiaray@gmail.com>
2022-05-14 11:43:18 +0200

    [KHYS-249] Add Header for fixing GetAll fonction

--- src/controllers/KhysInfoController.js
+++ src/controllers/KhysInfoController.js
@@ -4,9 +4,10 @@ const {
   transformIntValue,
   extractFieldsToChange,
   createSlug,
+  extractQueryParameters,
 } = require('./../utils/requestHandler');
 
-// create a user type
+// Create data for KhysInfo
 const createOne = async (req, res) => {
   try {
     checkRequiredFields(req, res, [
@@ -54,7 +55,7 @@ const createOne = async (req, res) => {
   }
 };
 
-// get user type by nameSlug field
+// Get 1 Id for KhysInfo
 const getOneById = async (req, res) => {
   try {
     const configClient = {
@@ -75,23 +76,33 @@ const getOneById = async (req, res) => {
     return res.status(400).json(error);
   }
 };
-
-// get all user type
+//  Get All datas for KhysInfo
 const getAll = async (req, res) => {
   try {
-    const KhysInfo = await Models.KhysInfo.findMany({});
+    let configClient = extractQueryParameters(req.query, [
+      'sort',
+      'range',
+      'filter',
+    ]);
+
+    const KhysInfo = await Models.KhysInfo.findMany(configClient);
+    const totalCount = await Models.KhysInfo.count(configClient);
 
     // The prisma client can run only 10 instances simultaneously,
     // so it is better to stop the current instance before sending the response
     await Models.$disconnect();
 
     // Success Response
+    res.header('Access-Control-Expose-Headers', 'Content-Range');
+    res.set('Content-Range', totalCount);
     res.status(200).json(KhysInfo);
   } catch (error) {
+    console.log(error);
     return res.status(400).json(error);
   }
 };
 
+//Update 1 data for KhysInfo
 const updateOne = async (req, res) => {
   try {
     const onlyThoseFields = [
@@ -131,7 +142,7 @@ const updateOne = async (req, res) => {
   }
 };
 
-// delete a user type
+//Delete 1 data for KhysInfo
 const deleteOne = async (req, res) => {
   try {
     //check juste entier

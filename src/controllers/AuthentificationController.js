6330f783ea812bc0689de2176c2ed8763e12dd6d
hassan <derkaouihassan@gmail.com>
2022-07-25 12:31:25 +0200

    refacto authentification files

---
+++ src/controllers/AuthentificationController.js
@@ -0,0 +1,89 @@
+require('dotenv').config()
+
+const Models = require('./../models');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+
+const login = async (req, res) => {
+    // Pas d'information à traiter
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({message: 'Erreur. Veuillez entrer l\'email et le mot de passe corrects'})
+    }
+
+    // Checking
+    const user = await Models.User.findUnique({
+        where: {
+            email: req.body.email
+        }
+    });
+
+    // Pas bon
+    if (!user) {
+        return res.status(400).json({message: 'Erreur. Identifiant ou mot de passe erroné'})
+    }
+
+    let token;
+    try {
+        //Creating jwt token
+        token = jwt.sign(
+            {userId: user.id, email: user.email},
+            "secretkeyappearshere",
+            {expiresIn: "3 hours"}
+        );
+    } catch (err) {
+        console.log(err);
+        const error = new Error("Error! Something went wrong.");
+        return next(error);
+    }
+}
+
+const signUp = async (req, res) => {
+    const users = await Models.User.findMany();
+
+    // Aucune information à traiter
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({message: 'Erreur. Veuillez entrer l\'email et le mot de passe'})
+    }
+
+    // Checking
+    const userExisting = await Models.User.findUnique({
+        where: {
+            email: req.body.email,
+        },
+    })
+
+    // Pas bon
+    if (userExisting) {
+        return res.status(400).json({message: `Error. Un utlisateur existe déjà avec cet adresse email`})
+    }
+
+    // Données du nouvel utilisateur
+    const cryptoPassword = bcrypt.hashSync(req.body.password, 12)
+    const newUser = await Models.User.create({
+        data: {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            age: parseInt(req.body.age),
+            cellphone: req.body.cellphone,
+            homephone: req.body.homephone,
+            email: req.body.email,
+            password: cryptoPassword,
+            user_type_id: req.body.user_type_id,
+            role: "USER",
+        },
+    })
+
+    return res.status(201).json({newUser, message: `User  created`})
+}
+
+const logout = async (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+}
+
+module.exports = {
+    login,
+    signUp,
+    logout,
+};
--- src/routes/authServer.js
+++ src/routes/authServer.js
@@ -1,95 +1,18 @@
-require('dotenv').config()
-
-
 // Initialize express server
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const Models = require('./../models');
-const bcrypt = require("bcrypt");
 const router = express.Router();
 
-let refreshTokens = []
+const { signUp, login, logout } = require('./../controllers/AuthentificationController');
 
 
 /* Formulaire de connexion */
-router.post('/login', async (req, res) => {
-    // Pas d'information à traiter
-    if (!req.body.email || !req.body.password) {
-        return res.status(400).json({message: 'Erreur. Veuillez entrer l\'email et le mot de passe corrects'})
-    }
-
-    // Checking
-    const user = await Models.User.findUnique({
-        where: {
-            email: req.body.email
-        }
-    });
-
-    // Pas bon
-    if (!user) {
-        return res.status(400).json({message: 'Erreur. Identifiant ou mot de passe erroné'})
-    }
-
-    let token;
-    try {
-        //Creating jwt token
-        token = jwt.sign(
-            { userId: user.id, email: user.email },
-            "secretkeyappearshere",
-            { expiresIn: "3 hours" }
-        );
-    } catch (err) {
-        console.log(err);
-        const error = new Error("Error! Something went wrong.");
-        return next(error);
-    }
-})
+router.post('/login', login)
 
 //inscription
-router.post('/signup', async (req, res) => {
-    const users = await Models.User.findMany();
-
-    // Aucune information à traiter
-    if (!req.body.email || !req.body.password) {
-        return res.status(400).json({message: 'Erreur. Veuillez entrer l\'email et le mot de passe'})
-    }
-
-    // Checking
-    const userExisting = await Models.User.findUnique({
-        where: {
-            email: req.body.email,
-        },
-    })
-
-    // Pas bon
-    if (userExisting) {
-        return res.status(400).json({message: `Error. Un utlisateur existe déjà avec cet adresse email`})
-    }
-
-    // Données du nouvel utilisateur
-    const cryptoPassword = bcrypt.hashSync(req.body.password, 12)
-    const newUser = await Models.User.create({
-        data: {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            age: parseInt(req.body.age),
-            cellphone: req.body.cellphone,
-            homephone: req.body.homephone,
-            email: req.body.email,
-            password: cryptoPassword,
-            user_type_id: req.body.user_type_id,
-            role: "USER",
-        },
-    })
-
-    return res.status(201).json({newUser, message: `User  created`})
-})
+router.post('/signup', signUp )
 
 //deconnexion
-router.delete('/logout', (req, res) => {
-    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-    res.sendStatus(204)
-})
+router.delete('/logout', logout)
 
 
 module.exports = router;

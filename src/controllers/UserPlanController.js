5724aa6780669646670c9e862bdd2622676c8235
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2022-07-27 20:00:38 +0200

    ajout des fonction pour les stats

--- src/controllers/UserPlanController.js
+++ src/controllers/UserPlanController.js
@@ -1,5 +1,6 @@
 const { isEmpty } = require('ramda');
 const Models = require('./../models');
+const {transformBooleanValue} = require("../utils/requestHandler");
 
 const createUserPlan = async (req, res) => {
 
@@ -63,8 +64,87 @@ const findById = async (req, res) => {
     }
 }
 
+const getNbUserPlans = async (req, res) => {
+    try {
+      const totalCount = await Models.userPlan.count({
+        where: {
+          isActive: transformBooleanValue(req.params.isActive)
+        }
+      });
+  
+      // The prisma client can run only 10 instances simultaneously,
+      // so it is better to stop the current instance before sending the response
+      await Models.$disconnect();
+  
+      // Add to ResponseHeaders the totalcount
+      res.status(200).json(totalCount);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+};
+
+const getCa = async (req, res) => {
+    try {
+        const userPlans = await Models.userPlan.findMany({
+            where: {
+                isActive: true
+            },
+            select:{
+                Plan:{
+                    select:{
+                        price:true
+                    }
+                }
+            }
+        })
+        let ca = 0;
+        userPlans.forEach((plan) => {
+            ca += plan.Plan.price
+        });
+        res.status(200).json(ca)
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+}
+
+const getLast = async (req, res) => {
+    const nb = 10;
+    try {
+        const userPlans = await Models.userPlan.findMany({
+            select:{
+                User:{
+                    select:{
+                        lastName:true,
+                        firstName:true,
+                        email:true
+                    }
+                },
+                Plan:{
+                    select:{
+                        name:true,
+                        price:true,
+                    }
+                },
+                billing_date:true,
+                id:true
+            },
+            orderBy:{
+                billing_date: "desc"
+            },
+        })
+        res.status(200).json(userPlans.slice(0, nb))
+
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json(error);
+    }
+}
+
 module.exports = {
     findAll,
     findById,
-    createUserPlan
+    createUserPlan,
+    getNbUserPlans,
+    getCa,
+    getLast
 }
--- src/routes/user_plan.js
+++ src/routes/user_plan.js
@@ -1,9 +1,13 @@
 var express = require('express');
-const { findAll, findById, createUserPlan} = require('../controllers/UserPlanController');
+const { findAll, findById, createUserPlan, getNbUserPlans, getCa, getLast} = require('../controllers/UserPlanController');
 var router = express.Router();
 
 router.get("/all", findAll);
+router.get('/ca', getCa);
+router.get("/last_orders", getLast)
+router.get("/count/:isActive", getNbUserPlans);
 router.get('/:id', findById);
 router.post("/new", createUserPlan);
+
 
 module.exports = router;
